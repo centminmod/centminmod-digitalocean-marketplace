6e78fc59c6652ef04555895d9a18f96d83eae802
George Liu <eva2000@centminmod.com>
2019-03-07 16:43:39 +1000

    update first-login.sh get & set emails

--- scripts/first-login.sh
+++ scripts/first-login.sh
@@ -15,13 +15,62 @@ echo "
 "
 }
 
+get_email() {
+  echo
+  echo "--------------------------------------------------------------------"
+  echo "Setup Server Administration Email"
+  echo "Emails will be used for future notification alert features"
+  echo "--------------------------------------------------------------------"
+  echo "Hit Enter To Skip..."
+  echo "Will be prompted everytime run centmin.sh if both emails not entered"
+  echo "--------------------------------------------------------------------"
+  read -ep "enter primary email: " mainemail
+  read -ep "enter secondary email: " secondemail
+  echo "--------------------------------------------------------------------"
+
+  if [ -z "$mainemail" ]; then
+    mainemail=""
+    rm -rf /etc/centminmod/email-primary.ini
+    echo "primary email setup skipped..."
+  else
+    echo
+    echo "Primary: $mainemail"
+    echo "$mainemail" > /etc/centminmod/email-primary.ini
+    echo "setup at /etc/centminmod/email-primary.ini"
+    echo
+    echo -n "  "
+    cat /etc/centminmod/email-primary.ini
+    echo
+    if [ -f "$(which git)" ]; then
+      git config --global user.email "$mainemail"
+      git config --global user.name "cmm-user"
+      # git config user.email
+      # git config user.name
+    fi
+  fi
+  if [ -z "$secondemail" ]; then
+    secondemail=$mainemail
+    rm -rf /etc/centminmod/email-secondary.ini
+    echo "secondary email setup skipped..."
+  else
+    echo "Secondary: $secondemail"
+    echo "$secondemail" > /etc/centminmod/email-secondary.ini
+    echo "setup at /etc/centminmod/email-secondary.ini"
+    echo
+    echo -n "  "
+    cat /etc/centminmod/email-secondary.ini
+    echo
+  fi
+  echo
+}
+
 whitelistip() {
   # whitelist ssh log in user's IP in csf firewall
   # https://centminmod.com/csf_firewall.html
   echo
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   echo "Whitelist IP in CSF Firewall"
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   ssh_user_ip=$(echo $SSH_CLIENT | awk '{print $1}')
   csf -a ${ssh_user_ip} # do-firstlogin-ip-whitelisting
   echo "${ssh_user_ip}" >> /etc/csf/csf.ignore
@@ -31,10 +80,10 @@ whitelistip() {
 
 set_hostname() {
   echo
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   echo "Setup main hostname as per Getting Started Guide Step 1"
   echo "https://centminmod.com/getstarted.html"
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   echo
   read -p "Enter desired hostname for this VPS: " yourhostname
   echo
@@ -48,18 +97,18 @@ set_hostname() {
 
 cmm_update() {
   echo
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   echo "Ensure centmin mod up to date"
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   cmupdate
   echo
 }
 
 reset_mysqlroot() {
   echo
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   echo "Generate mysql root password"
-  echo "----------------------------------------------------------"
+  echo "--------------------------------------------------------------------"
   if [ -f /root/.my.cnf ]; then
     echo "Previous MySQL root password:"
     echo
@@ -70,14 +119,14 @@ reset_mysqlroot() {
     echo "mysqladmin -u root -p${OLDMYSQLROOTPASS} password $NEWMYSQLROOTPASS"
     mysqladmin -u root -p${OLDMYSQLROOTPASS} password $NEWMYSQLROOTPASS
     echo
-    echo "----------------------------------------------------------"
+    echo "--------------------------------------------------------------------"
     echo "New MySQL root user password: $NEWMYSQLROOTPASS"
-    echo "----------------------------------------------------------"
+    echo "--------------------------------------------------------------------"
     echo
     sed -i "s|password=.*|password=$NEWMYSQLROOTPASS|" /root/.my.cnf
-    echo "----------------------------------------------------------"
+    echo "--------------------------------------------------------------------"
     echo "/root/.my.cnf updated"
-    echo "----------------------------------------------------------"
+    echo "--------------------------------------------------------------------"
     echo
     cat /root/.my.cnf
     echo
@@ -89,8 +138,9 @@ reset_bashrc() {
 }
 
 msg
+get_email
+set_hostname
 whitelistip
 cmm_update
-set_hostname
 reset_mysqlroot
 reset_bashrc
