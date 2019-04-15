592394a3f507e72a9484e0c6b0bcbade36382a58
George Liu <eva2000@centminmod.com>
2019-04-15 22:34:22 +1000

    ensure elrepo-kernel repo enabled if installed

--- packer/scripts/cmm-install.sh
+++ packer/scripts/cmm-install.sh
@@ -381,6 +381,7 @@ if [[ "$INSTALL_ELREPO" = [yY] ]]; then
   rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
   rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
   yum -y remove kernel-tools kernel-tools-libs
+  yum-config-manager --enable elrepo-kernel
   yum -y install kernel-ml kernel-ml-devel kernel-ml-tools --enablerepo=elrepo-kernel
   yum -y versionlock kernel-[0-9]*
   awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg
